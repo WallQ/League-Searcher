1081cf564166f5d64c6793b1f17aadaa5b05ca36
Sérgio Félix <sergio98@outlook.pt>
2022-08-02 01:04:47 +0100

    num to number

--- src/utils/numberToMastery.tsx
+++ src/utils/numberToMastery.tsx
@@ -1,13 +1,13 @@
-import Mastery1 from '../assets/images/mastery-1.png';
-import Mastery2 from '../assets/images/mastery-2.png';
-import Mastery3 from '../assets/images/mastery-3.png';
-import Mastery4 from '../assets/images/mastery-4.png';
-import Mastery5 from '../assets/images/mastery-5.png';
-import Mastery6 from '../assets/images/mastery-6.png';
-import Mastery7 from '../assets/images/mastery-7.png';
+import Mastery1 from '../assets/images/masteries/mastery-1.png';
+import Mastery2 from '../assets/images/masteries/mastery-2.png';
+import Mastery3 from '../assets/images/masteries/mastery-3.png';
+import Mastery4 from '../assets/images/masteries/mastery-4.png';
+import Mastery5 from '../assets/images/masteries/mastery-5.png';
+import Mastery6 from '../assets/images/masteries/mastery-6.png';
+import Mastery7 from '../assets/images/masteries/mastery-7.png';
 
-const numberToMastery = (num: number) => {
-	switch (num) {
+const numberToMastery = (number: number) => {
+	switch (number) {
 		case 1:
 			return Mastery1;
 		case 2:
